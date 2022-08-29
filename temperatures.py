288ec95ca0fbef47cabd73696786ec42c7b3a65d
N-Ceneviva <N-Ceneviva>
2022-08-29 15:15:11 +0000

    Added Cells

--- temperatures.py
+++ temperatures.py
@@ -28,3 +28,12 @@ display(df.select(roundedTemp("unit", "temp")))
 # COMMAND ----------
 
 display(df.select(convertFtoC("unit", "temp")))
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Adding a cell
+
+# COMMAND ----------
+
+
